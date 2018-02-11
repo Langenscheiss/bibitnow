9f38cbc0f25e9af61f2fe1f273d87da0f2fc28d3
someone <someone@someplace.com>
2018-02-11 03:26:53 +0100

    v0.80RC0a

--- extractors/prefselectors/0_EXAMPLE.js
+++ extractors/prefselectors/0_EXAMPLE.js
@@ -56,22 +56,17 @@ var BINPrefselector = ( function () {
 	 */
 	// this function is called by the background script in order to return a properly formatted citation download link
 	function formatCitationLink(metaData, link) {
-		var returnString = "";
 		link = link.trim();
-		if (link != null && link != "") {
-			returnString = metaData["citation_url"].match(/^(http[s]?:\/\/[^\/]*).*$/);
-			if (returnString != null && returnString.length > 1) {
-				if (metaData["query_summary"]["citation_download"] == 1) {
-					returnString = returnString[1] + link.replace(/exportCitation/,"getCitation") + "?citation-type=reference";
-				} else if (metaData["query_summary"]["citation_download"] == 2) {
- 					returnString = returnString[1] + link.replace(/documentcitationdownload/,"documentcitationdownloadformsubmit").replace(/publicationDoi[^&]+&/,"").replace(/&type=.*$/,"&fileFormat=PLAIN_TEXT&hasAbstract=CITATION");
-					metaData["citation_download_method"] = "POST";
-				}
-			} else {
-				returnString = "";
-			}
-		}
-		return returnString;
+		if (link == null || link == "") return "";
+		if (metaData["query_summary"]["citation_download"] == 1) {
+			link = link.replace(/exportCitation/,"getCitation") + "?citation-type=reference";
+			return (metaData["citation_url"].replace(/wiley\.com\/.*$/,"wiley.com") + link + "&download-citation-abstract=Citation+%26+Abstract");
+		} else if (metaData["query_summary"]["citation_download"] == 2) {
+			link = link.replace(/documentcitationdownload/,"documentcitationdownloadformsubmit").replace(/publicationDoi[^&]+&/,"").replace(/&type=.*$/,"&fileFormat=PLAIN_TEXT&hasAbstract=CITATION_AND_ABSTRACT");
+			metaData["citation_download_method"] = "POST";
+		}  
+		if (link == "") return "";
+		return (metaData["citation_url"].replace(/wiley\.com\/.*$/,"wiley.com") + link);
 	}
 	
 	/* The object prefselectorMsg contains the preferred css selectors used to query bibfield specific meta data. Here, one can add selectors to a set of standard selectors which are hardcoded into the meta extractor script. Any user defined selector is queried PRIOR to the standard selectors, hence the name "preferred selector". More specifically, prefselectorMsg is a name-index object of the format "bibfield: [ [css-selector, attribute], ...],". "bibfield" may be any of the 17 bibfields in the list above. The CSS selector selects specific html tags of the website's html source, and "attribute" is any html tag attribute (such as "href" for anchor tags a) or "innerText". The latter may be used to get the text between the selected html tags <tag> and </tag>. For each bibfield, several pairs of selector and attribute can be specified. The first one will be queried first, and so on. 
@@ -96,9 +91,10 @@ var BINPrefselector = ( function () {
 	var prefselectorMsg = { 
 		citation_author: [ ['meta[name="citation_authors"]','content'] ],
 		citation_firstpage: [ ['meta[name="citation_firstpage"]','content'] , ['p.issue-header__description span:last-child','innerText'] ],
-		citation_misc: [ ['meta[name="citation_book_title"]','content'] ],
+		citation_misc: [ ['meta[name="citation_book_title"]','content'] , ['section#abstract span.mjx-math, section#abstract span.MJX_Assistive_MathML','innerText',true, 1024, true, 1000] ],
 		citation_publisher: [ ['footer#copyright','innerText'] , ['p#copyright','innerText'] ],
-		citation_download: [ ['a.analytics-view-citation','href'] , ['li.citation a','href' ]]
+		citation_download: [ ['a.analytics-view-citation','href'] , ['li.citation a','href' ] ],
+		citation_abstract: [ ['section#abstract','innerText',true,20000] , ['div#abstract','innerText',true,20000] ]
 	};
 
 	// finally expose selector message
