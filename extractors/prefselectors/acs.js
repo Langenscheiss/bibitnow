435946798e8ef84f0098432d642925f9079c709f
someone <someone@someplace.com>
2018-02-19 01:40:52 +0100

    v0.80

--- extractors/prefselectors/acs.js
+++ extractors/prefselectors/acs.js
@@ -9,13 +9,17 @@ var BINPrefselector = ( function () {
 
 	// this function is called by the background script in order to return a properly formatted citation download link
 	function formatCitationLink(metaData, link) {
-		link = metaData["citation_url"].replace(/acs\.org\/.*$/,"acs.org") + link;
+		link = metaData["citation_url"].replace(/(^.*?[\/]+.*?)\/.*$/,
+			function(match, $1, offset, original) {
+				return $1;
+			}
+		) + link;
 		return link.replace(/showCitFormats/,"downloadCitation") + "&include=abs";
 	}
 	
 	// these are the preferred selectors used, and may be modified. The format is "bibfield: [ [css-selector,attribute], ...],", where "attribute" can be any html tag attribute or "innerText" to get the text between <tag> and </tag>
 	var prefselectorMsg = { 
-		citation_journal_title: [ ['div#series-logo img','alt'] ],
+		citation_journal_title: [ ['div#series-logo img','alt'] , ['input[name="quickLinkJournal"]','value']],
 		citation_volume: [ ['div#citation span.citation_volume','innerText'] ],
 		citation_misc: [ ['div#citation','innerText'] ],
 		citation_issn: [ ['a#rightsAndPermissions','href'] ],
